42396b5655b10873922acb0e0ce9cefc511c6075
dmtsamson <dmtsamson@mymail.mapua.edu.ph>
2025-11-07 09:42:42 +0900

    Initial commit - Angular frontend for LE5 Blog App

---
+++ src/app/components/register-page/register-page.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register-page',
+  templateUrl: './register-page.component.html',
+  styleUrls: ['./register-page.component.css']
+})
+export class RegisterPageComponent {
+  form: any = {
+    username: null,
+    password: null,
+    firstname: null,
+    lastname: null
+  }
+
+  constructor(private http: HttpClient, private route: Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit(): void {
+    const {
+      username, password, firstName, lastName
+    } = this.form
+
+    console.log(this.form);
+
+    this.http.post("https://localhost:7193/api/Login/register", this.form, {responseType: 
+      'text'}).subscribe(data => {
+        this.route.navigate(['/login'])
+      })
+    }
+  }
